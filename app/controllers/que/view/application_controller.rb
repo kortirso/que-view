612594a180f3cbbfe998c8f8ed2832e0bdd2423f
Bogdanov Anton <kortirso@gmail.com>
2023-11-15 19:21:18 +0300

    release 0.2.1

--- app/controllers/que/view/application_controller.rb
+++ app/controllers/que/view/application_controller.rb
@@ -3,14 +3,14 @@
 module Que
   module View
     class ApplicationController < ActionController::Base
-      http_basic_authenticate_with name: ::Que::Web.configuration.ui_username,
-                                   password: ::Que::Web.configuration.ui_password,
+      http_basic_authenticate_with name: ::Que::View.configuration.ui_username,
+                                   password: ::Que::View.configuration.ui_password,
                                    if: -> { basic_auth_enabled? }
 
       private
 
       def basic_auth_enabled?
-        configuration = ::Que::Web.configuration
+        configuration = ::Que::View.configuration
 
         return false if configuration.ui_username.blank?
         return false if configuration.ui_password.blank?
